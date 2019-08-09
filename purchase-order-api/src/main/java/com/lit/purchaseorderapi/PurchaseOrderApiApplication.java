554d4e50af4ab5d8b16511f5ce304f86b54111a3
Baird <n0301384@lm.lmig.com>
2019-08-09 18:28:07 +0100

    Initial Commit

---
+++ purchase-order-api/src/main/java/com/lit/purchaseorderapi/PurchaseOrderApiApplication.java
@@ -0,0 +1,13 @@
+package com.lit.purchaseorderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseOrderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseOrderApiApplication.class, args);
+	}
+
+}
